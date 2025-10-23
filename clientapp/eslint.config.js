d7d3dd52e353daa0775372b842a09e49fde55fc2
Nana Essandoh <nanaessandoh@gmail.com>
2025-10-23 07:27:15 -0400

    Fix eslint/ updated client app to vite/ update husky

---
+++ clientapp/eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import pluginReact from "eslint-plugin-react";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
+    plugins: { js },
+    languageOptions: { globals: globals.browser }
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    }
+  },
+  {
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  }
+]);
