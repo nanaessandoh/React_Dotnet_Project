f44c7a0291b131b192b1fdbaad2c3e6f8d77dc69
Nana Essandoh <nanaessandoh@gmail.com>
2025-12-02 06:02:11 -0500

    Fix issues with test

--- clientapp/src/app/layout/_test_/App.test.tsx
+++ clientapp/src/app/layout/_test_/App.test.tsx
@@ -4,7 +4,7 @@ import App from '../App';
 import renderWithClient from '../../../test-utils';
 
 describe('App Component', () => {
-  it('renders Vite + React heading', () => {
+  it('renders Homepage with Reactivites in Navbar', () => {
     const mockActivities = [
       { activityId: '1', title: 'Mock 1', description: '', category: '', city: '', venue: '', date: new Date().toISOString() },
     ];
--- clientapp/src/test-utils.tsx
+++ clientapp/src/test-utils.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MemoryRouter } from 'react-router';
 
 type InitialQuery = {
     queryKey: readonly unknown[];
@@ -19,7 +20,9 @@ export function renderWithClient(ui: React.ReactElement, options?: { initialQuer
     });
 
     const wrapper = ({ children }: { children?: React.ReactNode }) => (
-        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+        <QueryClientProvider client={queryClient}>
+            <MemoryRouter>{children}</MemoryRouter>
+        </QueryClientProvider>
     );
 
     return { ...render(ui, { wrapper }), queryClient };
