fe68132c2b9ce39a5cf4604a2cb327bea783407d
Nana Essandoh <nanaessandoh@gmail.com>
2026-01-26 06:09:54 -0500

    Add form controls

---
+++ clientapp/src/app/shared/components/DateTimeInput.tsx
@@ -0,0 +1,26 @@
+import { useController, UseControllerProps, FieldValues } from 'react-hook-form'
+import { DateTimePicker, DateTimePickerProps } from '@mui/x-date-pickers/DateTimePicker';
+
+type Props<T extends FieldValues> = {} & UseControllerProps<T> & DateTimePickerProps
+
+const DateTimeInput = <T extends FieldValues>(props: Props<T>) => {
+    const { field, fieldState } = useController({ ...props })
+    return (
+        <DateTimePicker
+            {...props}
+            {...field}
+            value={field.value ? new Date(field.value) : null}
+            onChange={value => field.onChange(new Date(value!))}
+            sx={{ width: '100%' }}
+            slotProps={{
+                textField: {
+                    onBlur: field.onBlur,
+                    error: !!fieldState.error,
+                    helperText: fieldState.error?.message
+                }
+            }}
+        />
+    )
+}
+
+export default DateTimeInput
