991d3b3518c7808fbc839b6b7fd90ec5e7c3d9bd
Nana Essandoh <nanaessandoh@gmail.com>
2026-02-02 06:24:14 -0500

    Add user controller endpoints

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,85 @@
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    public class UsersController : BaseApiController<UsersController>
+    {
+        private readonly SignInManager<Identity.Models.User> _signInManager;
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ILogger<UsersController> logger, IMediator mediator, SignInManager<Identity.Models.User> signInManager)
+        : base(logger, mediator)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] Identity.Write.User user)
+        {
+            return await TryAsync(async () =>
+            {
+                var newUser = new Identity.Models.User
+                {
+                    DisplayName = user.DisplayName,
+                    Email = user.Email,
+                    UserName = user.Email
+                };
+
+                var result = await _signInManager.UserManager.CreateAsync(newUser, user.Password);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+                    return Ok();
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+
+                return ValidationProblem();
+            });
+        }
+
+        [AllowAnonymous]
+        [HttpGet("user-info")]
+        public async Task<IActionResult> GetUserInfo()
+        {
+            return await TryAsync(async () =>
+            {
+                if (User.Identity?.IsAuthenticated == false)
+                {
+                    return NoContent();
+                }
+
+                var user = await _signInManager.UserManager.GetUserAsync(User);
+                if (user == null)
+                {
+                    return Unauthorized();
+                }
+
+                return Ok(new
+                {
+                    user.DisplayName,
+                    user.Email,
+                    user.UserName,
+                    user.Id,
+                    user.ImageUrl
+                });
+            });
+        }
+
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return NoContent();
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -55,6 +55,7 @@ namespace API
 
             app.UseCors(x => x.AllowAnyHeader()
                               .AllowAnyMethod()
+                              .AllowCredentials()
                               .WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
             app.UseAuthentication();
