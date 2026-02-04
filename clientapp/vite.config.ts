5624dcf08c55815e0e53ef123e8046a82a55ce1b
Nana Essandoh <nanaessandoh@gmail.com>
2026-02-04 05:15:21 -0500

    address ssl issues by downgrading to node v20.20.0

--- clientapp/vite.config.ts
+++ clientapp/vite.config.ts
@@ -1,12 +1,13 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
   server: {
     port: 3000
   },
-  plugins: [react()],
+  plugins: [basicSsl(), react()],
   test: {
     globals: true,
     environment: 'jsdom',
