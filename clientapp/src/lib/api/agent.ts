52ffee8ff0247dc9ca6e0768c8f6b1e30d797042
Nana Essandoh <nanaessandoh@gmail.com>
2026-02-17 07:50:17 -0500

    Remove sleep in agent

--- clientapp/src/lib/api/agent.ts
+++ clientapp/src/lib/api/agent.ts
@@ -4,12 +4,6 @@ import { store } from "../stores/store";
 import { toast } from "react-toastify";
 import { router } from "../../app/router/Routes";
 
-const sleep = (delay: number) => {
-    return new Promise((resolve) => {
-        setTimeout(resolve, delay);
-    });
-}
-
 const agent = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
@@ -22,12 +16,10 @@ agent.interceptors.request.use(config => {
 
 agent.interceptors.response.use(
     async response => {
-        await sleep(1000);
         store.uiStore.isIdle();
         return response;
     },
     async (error: AxiosError) => {
-        await sleep(1000);
         store.uiStore.isIdle();
         const { status, data } = error.response as any
         const requestUrl = error.config?.url;
